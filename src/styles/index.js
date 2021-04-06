e53435231ced142e5e3f34671ffdc74374ffdd68
Justin <jmerlo26@gmail.com>
2021-04-05 23:15:03 -0400

    appfairy: Migrate

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,18 +1,7 @@
 /* eslint-disable */
 
 const styles = [
-  {
-    type: 'href',
-    body: '/css/normalize.css',
-  },
-  {
-    type: 'href',
-    body: '/css/webflow.css',
-  },
-  {
-    type: 'href',
-    body: '/css/rally-2.webflow.css',
-  },
+
 ]
 
 const loadingStyles = styles.map((style) => {
--- src/views/index.js
+++ src/views/index.js
@@ -1,24 +1,5 @@
 /* eslint-disable */
 
-export { default as UnauthorizedView } from './UnauthorizedView'
-export { default as NotFoundView } from './NotFoundView'
-export { default as ChangelogView } from './ChangelogView'
-export { default as CheckoutView } from './CheckoutView'
-export { default as DetailCategoryView } from './DetailCategoryView'
-export { default as DetailForumTopicsView } from './DetailForumTopicsView'
-export { default as DetailHoldingsView } from './DetailHoldingsView'
-export { default as DetailLinkTagsView } from './DetailLinkTagsView'
-export { default as DetailMarketNavMenuView } from './DetailMarketNavMenuView'
-export { default as DetailMarketSectionPreviewView } from './DetailMarketSectionPreviewView'
-export { default as DetailNewsFiltersView } from './DetailNewsFiltersView'
-export { default as DetailProductView } from './DetailProductView'
-export { default as DetailSkuView } from './DetailSkuView'
-export { default as DetailTopMoversView } from './DetailTopMoversView'
-export { default as DetailTrendingAssetsView } from './DetailTrendingAssetsView'
-export { default as DetailTrendingNewsView } from './DetailTrendingNewsView'
-export { default as IndexView } from './IndexView'
-export { default as LicensingView } from './LicensingView'
-export { default as OrderConfirmationView } from './OrderConfirmationView'
-export { default as PaypalCheckoutView } from './PaypalCheckoutView'
+
 
 /* eslint-enable */
